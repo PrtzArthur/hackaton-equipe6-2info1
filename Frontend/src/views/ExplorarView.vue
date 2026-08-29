0b0e1df3324e70fca269524c42fa45fb9ff5daae
BrunFOliveira <brunofkliveira@gmail.com>
2026-08-29 19:05:36 -0300

    fix: corrigida as tags globais no banco com inserção manual #1

--- Frontend/src/views/ExplorarView.vue
+++ Frontend/src/views/ExplorarView.vue
@@ -214,14 +214,15 @@ main {
   height: 100vh;
   flex-grow: 1;
   padding: 0;
-  margin-left: 12vw;
-  width: calc(100% - 12vw);
+  margin-left: 14vw;
+  width: calc(100% - 14vw);
   position: fixed;
   top: 0;
   bottom: 0;
   right: 0;
   box-sizing: border-box;
   overflow: hidden;
+  overflow-x: hidden;
 }
 .explore-card {
   width: 100%;
